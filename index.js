a329b4cd725cfc767df910655bd3cc55ca47094d
Aravind-portfolio <araxaravind2524@gmail.com>
2025-01-26 18:03:29 +0530

    mail

---
+++ index.js
@@ -0,0 +1,47 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+    if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+    }
+    });
+});
+document.querySelectorAll('.animate').forEach(element => observer.observe(element));
+
+const preloader = document.getElementById("preloader");
+window.addEventListener("load", () => {
+    preloader.style.opacity = "0";
+    setTimeout(() => {
+        preloader.style.display = "none";
+    }, 500); 
+});
+
+emailjs.init('ekKGkOkEGo62YgrlD');
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const userEmail = document.getElementById('email').value;
+    const userMessage = document.getElementById('message').value;
+    const userName = document.getElementById('name').value;    
+
+    const templateParams = {
+        to_email: userEmail,
+        message: userMessage,
+        from_name: userName,
+        from_mail: userEmail,
+        to_name: 'Aravind'
+    };
+
+    emailjs.send('service_w58b0wy', 'template_snjecdv', templateParams)
+        .then(function(response) {
+            document.getElementById('response-message').textContent = "Message Sent Successfully!";
+            document.getElementById('response-message').style.color = "green";
+            console.log('Success!', response);
+        }, function(error) {
+            console.error('Error occurred:', error);
+            document.getElementById('response-message').textContent = "Oops! Something went wrong. Please try again.";
+            document.getElementById('response-message').style.color = "red";
+        });
+
+    document.getElementById('contact-form').reset();
+});
